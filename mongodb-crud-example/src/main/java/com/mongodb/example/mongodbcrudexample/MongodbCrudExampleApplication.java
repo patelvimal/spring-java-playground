25b45f3ee28e1cd5cffb6ed53dcdc23a097fad80
Vimal Patel <vimal.patel.nvs@gmail.com>
2021-05-26 20:27:13 +0530

    Initial commit

---
+++ mongodb-crud-example/src/main/java/com/mongodb/example/mongodbcrudexample/MongodbCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.example.mongodbcrudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbCrudExampleApplication.class, args);
+	}
+
+}
---
+++ mongodb-crud-example/src/test/java/com/mongodb/example/mongodbcrudexample/MongodbCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongodb.example.mongodbcrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
